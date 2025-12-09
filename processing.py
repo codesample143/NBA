8a93bd8355a4f2b88ea76284360364b29de3525a
= <definitelynotkevincs@gmail.com>
2025-12-09 15:56:12 -0500

    did some basic pyspark logic for distributed data processing

--- processing.py
+++ processing.py
@@ -2,4 +2,8 @@ import pyspark
 
 from pyspark.sql import SparkSession
 
+"""based on your knowledge of distributed architecture for spark you should understand this"""
 spark = SparkSession.builder.appName("ReadCSV").getOrCreate()
+
+df = spark.read.csv("nba_data/csv/game.csv")
+df.show(2)
